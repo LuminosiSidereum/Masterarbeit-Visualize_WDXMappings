e5bbaef49140890dab71ab902de46195994fed6d
Schulti <phillipp-schultheiss@web.de>
2024-11-29 20:34:16 +0100

    Doc-Strings für die Funktionen hinzugefügt

--- visualize_mappings.py
+++ visualize_mappings.py
@@ -7,6 +7,21 @@ import logging
 import time
 
 def visualize_mappings(dateiname: str, element:str):
+    """
+    Visualizes the mappings of a given element from a CSV file as a heatmap.
+    This function reads data from a specified CSV file, determines the value range and colormap 
+    based on the provided element, and generates a heatmap visualization. The heatmap is saved 
+    as both SVG and PNG files.
+    Parameters:
+        dateiname (str): The name of the CSV file (without extension) containing the data to be visualized.
+        element (str): The element to be visualized. Supported elements are "C", "N", "Fe", and "Zr".
+    Raises:
+        FileNotFoundError: If the specified CSV file is not found.
+        ValueError: If the specified element is not supported.
+    Returns:
+        None
+    """
+    
     # CSV-Datei einlesen (ohne Header und Index)
     try:
         data = pd.read_csv(f"data/{dateiname}.csv", header=None).values
@@ -58,12 +73,32 @@ def visualize_mappings(dateiname: str, element:str):
     plt.close()
     print(f"Visualisierung für {dateiname} ({element}) wurde erstellt.")
 
-def setup_output_directories():
+def setup_output_directories()->None:
+    """
+    Creates output directories if they do not already exist.
+    This function checks for the existence of the directories "svg-output" 
+    and "png-output" in the current working directory. If they do not exist, 
+    it creates them.
+    Returns:
+        None
+    """
+    
     # Erstelle Ausgabeordner, falls sie noch nicht existieren
     for directory in ["svg-output", "png-output"]:
         os.makedirs(directory, exist_ok=True)
         
 def main()->int:
+    """
+    Main function to visualize mappings from CSV files in the 'data' directory.
+    This function lists all files in the 'data' directory, filters for CSV files,
+    and calls the `visualize_mappings` function for each CSV file found. It counts
+    the number of CSV files processed and returns this count.
+    Raises:
+        FileNotFoundError: If the 'data' directory does not exist.
+    Returns:
+        int: The number of CSV files processed.
+    """
+    
     try:
         diretorycontent: list[str] = os.listdir("data") #type: ignore
     except FileNotFoundError:
